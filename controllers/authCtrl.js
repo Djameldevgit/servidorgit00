70a96d517a077a12144337c75906e0135a20610d
Djameldevgit <developpementwebdjamel@gmail.com>
2024-05-19 23:27:59 +0000

    kkkkkk

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -82,14 +82,15 @@ const authCtrl = {
       const isMatch = await bcrypt.compare(password, user.password)
       if (!isMatch) return res.status(400).json({ msg: "Le mot de passe est incorrect." })
 
-      const access_token = createAccessToken({ id: user._id })
-      const refresh_token = createRefreshToken({ id: user._id })
+      const access_token = createAccessToken({id: newUser._id})
+            const refresh_token = createRefreshToken({id: newUser._id})
 
+    
       res.cookie('refreshtoken', refresh_token, {
         httpOnly: true,
         path: '/api/refresh_token',
-        maxAge: 30 * 24 * 60 * 60 * 1000 // 30days
-      })
+        maxAge: 30*24*60*60*1000 // 30days
+    })
 
       res.json({
         msg: 'Login Success!',
@@ -288,29 +289,29 @@ const authCtrl = {
 
   generateAccessToken: async (req, res) => {
     try {
-        const rf_token = req.cookies.refreshtoken
-        if(!rf_token) return res.status(400).json({msg: "Please login now."})
+      const rf_token = req.cookies.refreshtoken
+      if (!rf_token) return res.status(400).json({ msg: "Please login now." })
 
-        jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, async(err, result) => {
-            if(err) return res.status(400).json({msg: "Please login now."})
+      jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, async (err, result) => {
+        if (err) return res.status(400).json({ msg: "Please login now." })
 
-            const user = await Users.findById(result.id).select("-password")
-            .populate('followers following', 'avatar username fullname followers following')
+        const user = await Users.findById(result.id).select("-password")
+          .populate('followers following', 'avatar username fullname followers following')
 
-            if(!user) return res.status(400).json({msg: "This does not exist."})
+        if (!user) return res.status(400).json({ msg: "This does not exist." })
 
-            const access_token = createAccessToken({id: result.id})
+        const access_token = createAccessToken({ id: result.id })
 
-            res.json({
-                access_token,
-                user
-            })
+        res.json({
+          access_token,
+          user
         })
-        
+      })
+
     } catch (err) {
-        return res.status(500).json({msg: err.message})
+      return res.status(500).json({ msg: err.message })
     }
-}
+  }
 }
 
 
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,7 +5,7 @@ const auth = require('../middleware/auth');
 router.post('/register', authCtrl.register);
 router.post('/login', authCtrl.login);
 router.post('/logout', authCtrl.logout);
-router.post('/refresh_token', authCtrl.generateAccessToken);
+router.post('/refresh_token', authCtrl.generateAccessToken)
 router.post('/login_sms', authCtrl.loginSMS);
 router.post('/sms_verify', authCtrl.smsVerify);
 router.post('/activation', authCtrl.activateEmail);
