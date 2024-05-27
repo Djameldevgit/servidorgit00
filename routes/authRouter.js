9843de6081f0af7249cecaa8c0f09f44de7a8ad9
Djameldevgit <developpementwebdjamel@gmail.com>
2024-05-27 22:38:59 +0000

    uuy

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,16 +1,21 @@
-const router = require('express').Router();
-const authCtrl = require('../controllers/authCtrl');
-const auth = require('../middleware/auth');
+const router = require('express').Router()
+const authCtrl = require('../controllers/authCtrl')
+const auth = require('../middleware/auth')
 
-router.post('/register', authCtrl.register);
-router.post('/login', authCtrl.login);
-router.post('/logout', authCtrl.logout);
-router.post('/refresh_token', authCtrl.generateAccessToken)
-router.post('/login_sms', authCtrl.loginSMS);
-router.post('/sms_verify', authCtrl.smsVerify);
-router.post('/activation', authCtrl.activeAccount);
-router.post('/forgot', authCtrl.forgotPassword);
-router.post('/reset', auth, authCtrl.resetPassword);
 
+
+router.post('/register', authCtrl.register)
+
+router.post('/login', authCtrl.login)
+
+router.post('/logout', authCtrl.logout)
  
-module.exports = router;
+router.post('/refresh_token', authCtrl.generateAccessToken)
+ 
+router.post('/activation', authCtrl.activateEmail);
+router.post('/forgot', authCtrl.forgotPassword)
+router.post('/reset', auth, authCtrl.resetPassword)
+
+
+
+module.exports = router
