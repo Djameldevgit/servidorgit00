9663d57b5b6d28dad328a33cde55857f1dbf21ed
Djameldevgit <developpementwebdjamel@gmail.com>
2024-05-31 00:13:54 +0000

    iuy

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ mongoose.connect(URI, {
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('clientegit00/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'cliente00', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'clientegit00', 'build', 'index.html'));
     });
 }
 
